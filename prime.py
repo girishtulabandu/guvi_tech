a253eefbcd3201e9397bd9c2fe061ad4e7836e5a
GIRISH <girishtulabandu66@gmail.com>
2018-07-04 17:51:07 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,12 @@
+n=int(input())
+count=0
+if(n<=1000):
+    for i in range(1,n+1):
+        if(n%i==0):
+            count=count+1
+    if(count>2):
+        print("no")
+    else:
+        print("yes")
+else:
+    print("Invalid")
