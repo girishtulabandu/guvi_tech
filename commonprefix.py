5b24a702c2d5fabec597618595240069e2650b7d
GIRISH <girishtulabandu66@gmail.com>
2018-07-06 13:40:58 +0530

    Create commonprefix.py

---
+++ commonprefix.py
@@ -0,0 +1,14 @@
+i=int(input())
+l=[]
+for x in range(i):
+    j=input("")
+    l.append(j)
+b=[]
+for i in range(len(l)):
+    b.append(list(l[i]))
+for k in range(i):
+    for h in range(len(b[k])):
+        if(b[k][h]==b[k+1][h]):
+            print(b[k][h],end="")
+        else:
+            break
