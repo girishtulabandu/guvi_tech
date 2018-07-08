eb9a9e22debee84bda50221fbef5be34124f4645
GIRISH <girishtulabandu66@gmail.com>
2018-07-08 23:44:32 +0530

    Create max_element.c

---
+++ max_element.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int arr[20],i,n,max=-9999;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	   scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	   if(arr[i]>max)
+	   {
+		max=arr[i];
+	   }
+	}
+	printf("%d",max);
+	return 0;
+	
+}
