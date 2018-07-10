abbfd4ef9b95635270253be2e4fe5db277bdc325
GIRISH <girishtulabandu66@gmail.com>
2018-07-10 09:55:32 +0530

    Update commonprefix.py

--- commonprefix.py
+++ commonprefix.py
@@ -1,12 +1,12 @@
 i=int(input())
 l=[]
-for r in range(i):
+for x in range(i):
     j=input("")
     l.append(j)
 b=[]
 for i in range(len(l)):
     b.append(list(l[i]))
-for k in range(i):
+for k in range(1):
     for h in range(len(b[k])):
         if(b[k][h]==b[k+1][h]):
             print(b[k][h],end="")
