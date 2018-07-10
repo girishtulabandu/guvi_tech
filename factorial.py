6ce47daf988242522b71ecde18102a08204ee13e
GIRISH <girishtulabandu66@gmail.com>
2018-07-10 11:57:17 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,8 @@
 N=int(input())
-if(N<=20):
-    def factorial(N):
-        if(N==0):
-            return 1
-        else:
-            return N*factorial(N-1)
-else:
-    print("Given number should be less than or equal to 20")
-print(factorial(N))
+def factorial(N):
+    if(N==0):
+        return 1
+    else:
+        return N*factorial(N-1)
 
+print(factorial(N))
