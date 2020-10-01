59fa1a00bd2b32531514a46ea5de1596a27c7040
Priyanshu Singh <62669697+priyanshusingh0610@users.noreply.github.com>
2020-10-01 17:22:16 +0530

    Update factorial.py
    Just a 4 line of code prints Factorial of any Positive Integer.

--- factorial.py
+++ factorial.py
@@ -1,8 +1,14 @@
-r=int(input())
-def factorial(r):
-    if(r==0):
-        return 1
-    else:
-        return r*factorial(r-1)
+# r=int(input())
+# def factorial(r):
+#     if(r==0):
+#         return 1
+#     else:
+#         return r*factorial(r-1)
 
-print(factorial(r))
+# print(factorial(r))
+
+
+import functools
+factorial = lambda n: functools.reduce(lambda x, y: x * y, range(1, n+1))
+num=int(input('Enter any Integer: '))
+print(factorial(num))
