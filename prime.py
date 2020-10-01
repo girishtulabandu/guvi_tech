626a330bdaa4405e9d8be899b105d465a4855a02
ndeoligence <ndeoligence@users.noreply.github.com>
2020-10-01 23:14:42 +0200

    Update prime.py
    -ve numbers & non-primes output No
    non-integers output Invalid
    primes output Yes

--- prime.py
+++ prime.py
@@ -1,12 +1,28 @@
-n=int(input())
-count=0
-if(n>0):
-    for i in range(1,n+1):
-        if(n%i==0):
-            count=count+1
-    if(count>2):
-        print("no")
-    else:
-        print("yes")
-else:
-    print("Invalid")
+import sys
+from math import ceil, sqrt
+
+
+def is_prime(n: int) -> bool:
+    """Returns True iff n is prime."""
+    if n == 2:
+        return True
+    elif n < 2 or n % 2 == 0:
+        return False
+
+    limit = ceil(sqrt(n))
+
+    for factor in range(3, limit + 1, 2):
+        if n % factor == 0:
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+    try:
+        num = int(input("Check prime: "))
+    except ValueError:
+        print(f"Invalid")
+        sys.exit(1)
+
+    print('Yes' if is_prime(num) else 'No')
