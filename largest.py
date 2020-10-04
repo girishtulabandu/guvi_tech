fda559489c610bf1bcb647d8a8c16b542895d8b3
am401 <andras.marton@wpengine.com>
2020-10-04 00:38:08 -0500

    Refactored code for largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,16 @@
-i=int(input())
-j=int(input())
-k=int(input())
-if(i>=j and i>=k):
-    print(i)
-elif(j>=i and j>=k):
-    print(j)
-else:
-    print(k)
+numbers = list()
+i = 0
+print("Please provide three numbers: ")
+while i < 3:
+    try:
+        n = int(input())
+        numbers.append(n)
+
+        i = i + 1
+    except ValueError:
+        print("Invalid number entered. Please only enter integers.")
+        i = 3
+
+if len(numbers) == 3:
+    largest_number = max(numbers)
+    print("The largest number given is: ", largest_number)
